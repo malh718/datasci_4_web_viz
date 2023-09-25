ccab00b679d7a132a88bcbe026e20382c178ec47
maliha <you@example.com>
2023-09-25 22:25:23 +0000

    m

---
+++ flaskapp/flaskapp.py
@@ -0,0 +1,48 @@
+from flask import Flask, render
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+
+
+def load_data():
+    url = "dele.csv"
+    return pd.read_csv(url)
+
+df = load_data()
+df_binge = df[(df['MeasureId'] == 'BINGE') & (df['Data_Value_Type'] == 'Age-adjusted prevalence')]
+
+counties = df_binge['LocationName'].unique()
+
+app_ui = ui.page_fluid(
+    ui.input_select("county", "Select County", {county: county for county in counties}),
+    ui.output_text_verbatim("avg_data_value"),
+    ui.output_plot("bar_chart")
+)
+
+def server(input, output, session):
+
+    @output
+    @render.text
+    def avg_data_value():
+        selected_county = input.county()
+        avg_value = df_binge[df_binge['LocationName'] == selected_county]['Data_Value'].mean()
+        return f"Average Binge Drinking Age-adjusted Prevalence for {selected_county}: {avg_value:.2f}%"
+
+    @output
+    @render.plot(alt="Binge Drinking Age-adjusted Prevalence Bar Chart")
+    def bar_chart():
+        overall_avg = df_binge['Data_Value'].mean()
+        selected_county_avg = df_binge[df_binge['LocationName'] == input.county()]['Data_Value'].mean()
+
+        fig, ax = plt.subplots(figsize=(10, 6))
+        ax.bar(['Selected County', 'Overall Average'], [selected_county_avg, overall_avg], color=['lightcoral', 'dodgerblue'])
+        
+        ax.set_ylabel('Data Value (Age-adjusted prevalence) - Percent')
+        ax.set_ylim(0, 30)
+        ax.set_title('Binge Drinking Age-adjusted Prevalence Comparison')
+        
+        return fig
+
+
+app = App(app_ui, server)
+
